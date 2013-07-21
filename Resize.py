4ab75ec7028fc13a56b5b50a2118ff9473106ee2
Akif Khan <akif500@gmail.com>
2013-07-22 00:28:35 +0530

    ext2 resizing done

--- Resize.py
+++ Resize.py
@@ -5,24 +5,49 @@ volume = "/dev/test/testlv2"
 
 def increase(volume,size,unit):
 	
-	p = subprocess.Popen(['blkid','-s','TYPE',volume]
-						,stdout=subprocess.PIPE)						
+	# Fetching the Filesystem of Volume
+	
+	p = subprocess.Popen(['blkid','-s','TYPE',volume],stdout=subprocess.PIPE)						
 						
 	# SHELL command to get filesystem of a disk is " blkid -s TYPE /dev/test/testlv 
 	
 	FILESYSTEM, err = p.communicate()
 	
-	print FILESYSTEM
 	
 	if 'ext2' in FILESYSTEM:
-		print "lolo"
-		p1 = subprocess.Popen(['lvextend','-L','+',size,unit,volume]
-							,stdout=subprocess.PIPE)
-					
-		output, err = p1.communicate()
+		
+		p1 = subprocess.Popen(['lvextend','-L+1G',volume],stdout=subprocess.PIPE)
+		
+		output, error = p1.communicate()
 	
+		if output:
+			print output
+			
+		if error:
+			print error	
+			
+	# Resizing Filesystem
+	
+	p1=subprocess.Popen(['umount',volume],stdout=subprocess.PIPE)
+	
+	output, error = p1.communicate()
+	
+			
+	p1=subprocess.Popen(['resize2fs',volume],stdout=subprocess.PIPE)
+	
+	output, error = p1.communicate()
+	
+	
+	p1=subprocess.Popen(['mount',volume,'/mnt'],stdout=subprocess.PIPE)
+	
+	output, error = p1.communicate()
+	
+	if output:
 		print output
-		print err
+			
+	if error:
+		print error
 	
-
-increase(volume,10,'M')
+	
+increase(volume,1,'G')
+#fsadm to grow the file system online.  ext4
