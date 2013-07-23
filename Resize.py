d5f6d37177bec7cf27971a8822f71934c5cd9a6b
Akif Khan <akif500@gmail.com>
2013-07-23 23:56:35 +0530

    commit

--- Resize.py
+++ Resize.py
@@ -1,53 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  Resize.py
+#  
+#  Copyright 2013 akif khan <akif500@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
 import subprocess
 
 volume = "/dev/test/testlv2"
 
 
 def increase(volume,size,unit):
-	
+
 	# Fetching the Filesystem of Volume
-	
-	p = subprocess.Popen(['blkid','-s','TYPE',volume],stdout=subprocess.PIPE)						
-						
-	# SHELL command to get filesystem of a disk is " blkid -s TYPE /dev/test/testlv 
-	
+
+	p = subprocess.Popen(['blkid','-s','TYPE',volume],stdout=subprocess.PIPE)
+
+	# SHELL command to get filesystem of a disk is " blkid -s TYPE /dev/test/testlv
+
 	FILESYSTEM, err = p.communicate()
-	
-	
+
 	if 'ext2' in FILESYSTEM:
-		
+
 		p1 = subprocess.Popen(['lvextend','-L+1G',volume],stdout=subprocess.PIPE)
-		
+
 		output, error = p1.communicate()
-	
+
 		if output:
 			print output
-			
+
 		if error:
-			print error	
-			
-	# Resizing Filesystem
-	
-	p1=subprocess.Popen(['umount',volume],stdout=subprocess.PIPE)
-	
-	output, error = p1.communicate()
-	
-			
-	p1=subprocess.Popen(['resize2fs',volume],stdout=subprocess.PIPE)
-	
-	output, error = p1.communicate()
-	
-	
-	p1=subprocess.Popen(['mount',volume,'/mnt'],stdout=subprocess.PIPE)
-	
-	output, error = p1.communicate()
-	
-	if output:
-		print output
-			
-	if error:
-		print error
-	
-	
+			print error
+
+		# Resizing Filesystem
+
+		p1=subprocess.Popen(['umount',volume],stdout=subprocess.PIPE)
+
+		output, error = p1.communicate()
+
+
+		p1=subprocess.Popen(['resize2fs',volume],stdout=subprocess.PIPE)
+
+		output, error = p1.communicate()
+
+
+		p1=subprocess.Popen(['mount',volume,'/mnt'],stdout=subprocess.PIPE)
+
+		output, error = p1.communicate()
+
+		if output:
+			print output
+		
+		if error:
+			print error
+
+
 increase(volume,1,'G')
 #fsadm to grow the file system online.  ext4
